a6379549ffabb5352f64b37a6d6025e3700c3d38
Heath Moore <heathjhmoore@gmail.com>
2020-01-30 21:01:02 -0600

    formatting productDetails section two

--- src/Components/ProductDetails/ProductDetails.js
+++ src/Components/ProductDetails/ProductDetails.js
@@ -23,7 +23,26 @@ class ProductDetails extends React.Component {
                     The first info will go in here
                 </div>
                 <div className="productDetailSection productDetailSectionTwo">
-                    The second info will go in here
+                    <div className="productDetailSectionTwoRow">
+                        <div>
+                            <h4>Maximum Price</h4>
+                            <p>{this.state.ProductDetails.maxPrice}</p>
+                        </div>
+                        <div>
+                            <h4>Minimum Price</h4>
+                            <p>{this.state.ProductDetails.minPrice}</p>
+                        </div>
+                    </div>
+                    <div className="productDetailSectionTwoRow">
+                        <div>
+                            <h4>Average Price</h4>
+                            <p>{this.state.ProductDetails.maxPrice}</p>
+                        </div>
+                        <div>
+                            <h4>Time On Clearance</h4>
+                            <p>{this.state.ProductDetails.minPrice}</p>
+                        </div>
+                    </div>
                 </div>
                 <div className="productDetailSection productDetailSectionThree">
                     The third info will go in here
